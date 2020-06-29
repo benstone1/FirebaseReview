cf30a6e2e4895e653149eee603663bdee6ba2053
Ben Stone <benjamin.h.stone@gmail.com>
2020-06-29 14:56:15 -0400

    Initial Commit

---
+++ FirebaseReviewLesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseReviewLesson
+//
+//  Created by Benjamin Stone on 6/29/20.
+//  Copyright © 2020 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
