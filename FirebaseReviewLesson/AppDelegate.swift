d79ea676c0d9e5207ed423d8dc099f718cfc3219
Ben Stone <benjamin.h.stone@gmail.com>
2020-06-29 15:00:52 -0400

    pod install

--- FirebaseReviewLesson/AppDelegate.swift
+++ FirebaseReviewLesson/AppDelegate.swift
@@ -4,9 +4,10 @@
 //
 //  Created by Benjamin Stone on 6/29/20.
 //  Copyright © 2020 Benjamin Stone. All rights reserved.
-//
+
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
